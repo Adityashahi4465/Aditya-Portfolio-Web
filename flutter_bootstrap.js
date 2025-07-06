d5e4ca8ad73e829745978e493c33522d7e0fc7bc
Aditya Shahi <adityakmcs@gmail.com>
2025-07-06 17:23:45 +0530

    added kavyakunj project

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3833918342"
+    serviceWorkerVersion: "1501266617"
   }
 });
