83cca8c2ce967894dcb87e1dc5d996586d785997
Aditya Shahi <adityakmcs@gmail.com>
2025-07-06 17:10:03 +0530

    d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,10 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-<<<<<<< HEAD
-    serviceWorkerVersion: "2972846180"
-=======
-    serviceWorkerVersion: "4066338718"
->>>>>>> 4db10d8 (f)
+    serviceWorkerVersion: "3833918342"
   }
 });
